aba7558da0ceea5a388f571126baf839cb5c0362
Aaditya18SP <aaditya.sp@somaiya.edu>
2023-04-03 18:41:30 +0530

    added guess the number

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let obj= {name: "aaditya", eng:40, hindi : 50};
-let id= document.getElementById("first");
-for(x in obj){
-    id.innerHTML=x;
+let obj= {name :"aaditya", eng : 40, hindi : 50};
+let id = document.getElementById("first");
+for(let x in obj){
+
+    id.innerHTML += (x+" " + obj[x]+ "<br>");
 }
